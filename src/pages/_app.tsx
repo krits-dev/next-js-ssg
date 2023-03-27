2cc784a27ea3eb8e7814387158476644e5f2cb2b
krits-dev <krits84@gmail.com>
2023-03-27 06:56:48 +0300

    codemod

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
-import '@/styles/globals.css'
+import React from 'react'
 import type { AppProps } from 'next/app'
+import '@/assets/scss/global.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
